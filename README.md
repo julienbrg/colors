4568b5c3001f2b7aa8f536508bd13a5c8f7e5f71
Julien Béranger <julien@strat.cc>
2025-03-20 08:50:05 +0100

    Add black image (#2)
    * add zhankai
    
    * add license
    
    * add alpha
    
    * fix readme

--- README.md
+++ README.md
@@ -1,66 +1,31 @@
-## Foundry
+# Colors
 
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
+A minimalist Solidity contract for RGB color management on EVM chains.
 
-Foundry consists of:
+## Motivation
 
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
+Storing colors efficiently is essential for on-chain art. This contract uses a packed uint24 representation (matching web hexadecimal format) to minimize gas costs while maintaining full RGB color support.
 
-## Documentation
+## Install
 
-https://book.getfoundry.sh/
-
-## Usage
-
-### Build
-
-```shell
-$ forge build
+```bash
+git clone https://github.com/yourusername/colors.git
+cd colors
+forge install
 ```
 
-### Test
+## Deploy
 
-```shell
-$ forge test
+```bash
+# Local development
+forge script script/DeployColors.s.sol --fork-url http://localhost:8545 --broadcast
+
+# Testnet/Mainnet
+forge script script/DeployColors.s.sol --rpc-url <YOUR_RPC_URL> --private-key <YOUR_PRIVATE_KEY> --broadcast
 ```
 
-### Format
+## Support
 
-```shell
-$ forge fmt
-```
+Feel free to reach out to [Julien](https://github.com/julienbrg) on [Farcaster](https://warpcast.com/julien-), [Element](https://matrix.to/#/@julienbrg:matrix.org), [Status](https://status.app/u/iwSACggKBkp1bGllbgM=#zQ3shmh1sbvE6qrGotuyNQB22XU5jTrZ2HFC8bA56d5kTS2fy), [Telegram](https://t.me/julienbrg), [Twitter](https://twitter.com/julienbrg), [Discord](https://discordapp.com/users/julienbrg), or [LinkedIn](https://www.linkedin.com/in/julienberanger/).
 
-### Gas Snapshots
-
-```shell
-$ forge snapshot
-```
-
-### Anvil
-
-```shell
-$ anvil
-```
-
-### Deploy
-
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
-
-### Cast
-
-```shell
-$ cast <subcommand>
-```
-
-### Help
-
-```shell
-$ forge --help
-$ anvil --help
-$ cast --help
-```
+<img src="https://bafkreid5xwxz4bed67bxb2wjmwsec4uhlcjviwy7pkzwoyu5oesjd3sp64.ipfs.w3s.link" alt="built-with-ethereum-w3hc" width="100"/>
--- test/Colors.t.sol
+++ test/Colors.t.sol
@@ -1,31 +1,20 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "../src/Colors.sol";
+import "../src/FavoriteColors.sol";
 
 contract ColorsTest is Test {
     Colors public colors;
+    FavoriteColors public favoriteColors;
 
     function setUp() public {
         colors = new Colors();
+        favoriteColors = new FavoriteColors();
     }
 
-    function testPredefinedColors() public view {
-        // Test predefined colors
-        assertEq(colors.WHITE(), 16777215);
-        assertEq(colors.BLACK(), 0);
-        assertEq(colors.PURPLE(), 9182340);
-        assertEq(colors.BLUE(), 4561656);
-
-        // Test getting colors by name
-        assertEq(colors.getPredefinedColor("WHITE"), colors.WHITE());
-        assertEq(colors.getPredefinedColor("BLACK"), colors.BLACK());
-        assertEq(colors.getPredefinedColor("PURPLE"), colors.PURPLE());
-        assertEq(colors.getPredefinedColor("BLUE"), colors.BLUE());
-    }
-
-    function testPackingAndUnpacking() public view {
+    function testBaseColorFunctionality() public {
         // Test packing
         uint24 testColor = colors.packColor(255, 128, 64);
         assertEq(testColor, 16744512); // 0xFF8040
@@ -40,21 +29,51 @@ contract ColorsTest is Test {
         assertEq(r, 255);
         assertEq(g, 128);
         assertEq(b, 64);
-    }
 
-    function testCustomColors() public {
         // Test adding a custom color
         colors.addCustomColor("ORANGE", 255, 165, 0);
         assertEq(colors.customColors("ORANGE"), 16753920); // 0xFFA500
     }
 
-    // Testing a simpler version without the string conversion
-    function testColorValues() public view {
-        uint24 purpleValue = colors.PURPLE();
+    function testPredefinedColors() public view {
+        // Test predefined colors
+        assertEq(favoriteColors.WHITE(), 16777215);
+        assertEq(favoriteColors.BLACK(), 0);
+        assertEq(favoriteColors.PURPLE(), 9182340);
+        assertEq(favoriteColors.BLUE(), 4561656);
 
-        // Test individual components instead
-        assertEq(colors.unpackRed(purpleValue), 140); // 0x8c
-        assertEq(colors.unpackGreen(purpleValue), 28); // 0x1c
-        assertEq(colors.unpackBlue(purpleValue), 132); // 0x84
+        // Test getting colors by name
+        assertEq(favoriteColors.getPredefinedColor("WHITE"), favoriteColors.WHITE());
+        assertEq(favoriteColors.getPredefinedColor("BLACK"), favoriteColors.BLACK());
+        assertEq(favoriteColors.getPredefinedColor("PURPLE"), favoriteColors.PURPLE());
+        assertEq(favoriteColors.getPredefinedColor("BLUE"), favoriteColors.BLUE());
+    }
+
+    function testFavoriteColorFunctionality() public {
+        // Test adding a custom color to the FavoriteColors contract
+        favoriteColors.addCustomColor("ORANGE", 255, 165, 0);
+        assertEq(favoriteColors.customColors("ORANGE"), 16753920); // 0xFFA500
+    }
+
+    function testColorDescriptions() public view {
+        // Test color descriptions
+        assertEq(favoriteColors.getColorDescription(favoriteColors.WHITE()), "White (0xFFFFFF)");
+        assertEq(favoriteColors.getColorDescription(favoriteColors.BLACK()), "Black (0x000000)");
+        assertEq(favoriteColors.getColorDescription(favoriteColors.PURPLE()), "Purple (0x8c1c84)");
+        assertEq(favoriteColors.getColorDescription(favoriteColors.BLUE()), "Blue (0x45a2f8)");
+
+        // Test description for a custom color
+        uint24 customColor = favoriteColors.packColor(100, 100, 100);
+        assertEq(favoriteColors.getColorDescription(customColor), "Custom color");
+    }
+
+    // Testing the individual components of colors
+    function testPurpleColorValues() public view {
+        uint24 purpleValue = favoriteColors.PURPLE();
+
+        // Test individual components
+        assertEq(favoriteColors.unpackRed(purpleValue), 140); // 0x8c
+        assertEq(favoriteColors.unpackGreen(purpleValue), 28); // 0x1c
+        assertEq(favoriteColors.unpackBlue(purpleValue), 132); // 0x84
     }
 }
